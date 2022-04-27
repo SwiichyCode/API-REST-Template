ae13445a99b274558e699251ccc662f1a41dfc0d
Swiichy <adlpromail@gmail.com>
2022-04-27 07:08:35 +0200

    add gestion err

--- src/routes/updatePokemon.js
+++ src/routes/updatePokemon.js
@@ -5,11 +5,21 @@ module.exports = (app) => {
     const id = req.params.id;
     Pokemon.update(req.body, {
       where: { id: id },
-    }).then((_) => {
-      Pokemon.findByPk(id).then((pokemon) => {
-        const message = `Le pokémon ${pokemon.name} a bien été modifié.`;
-        res.json({ message, data: pokemon });
+    })
+      .then((_) => {
+        return Pokemon.findByPk(id).then((pokemon) => {
+          if (pokemon === null) {
+            const message =
+              "Le pokemon demandé n'existe pas. Réessayez avec un autre identifiant";
+            return res.status(404).json({ message });
+          }
+          const message = `Le pokémon ${pokemon.name} a bien été modifié.`;
+          res.json({ message, data: pokemon });
+        });
+      })
+      .catch((error) => {
+        const message = `La liste des pokemons n'a pas pu être récupérée. Réessayez dans quelquels instants`;
+        res.status(500).json({ message, data: error });
       });
-    });
   });
 };
