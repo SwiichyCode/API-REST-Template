be996d02f10c5839c2949fc9415631716a965eb1
Swiichy <adlpromail@gmail.com>
2022-04-27 17:56:55 +0200

    Finalisation de la gestion d'erreur / contrainte d'unicite

---
+++ README.md
@@ -0,0 +1,2 @@
+https://www.youtube.com/watch?v=NRxzvpdduvQ&t=19291s / IMPORTANT
+https://www.youtube.com/watch?v=NRxzvpdduvQ&t=19291s / CONTRAINTE D'UNICITE
--- src/models/pokemon.js
+++ src/models/pokemon.js
@@ -1,3 +1,15 @@
+const validTypes = [
+  "Plante",
+  "Poison",
+  "Feu",
+  "Eau",
+  "Insecte",
+  "Vol",
+  "Normal",
+  "Electrik",
+  "Fée",
+];
+
 module.exports = (sequelize, DataTypes) => {
   return sequelize.define(
     "Pokemon",
@@ -11,18 +23,62 @@ module.exports = (sequelize, DataTypes) => {
       name: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique: {
+          msg: "Le nom est déjà pris.",
+        },
+        validate: {
+          notEmpty: {
+            msg: "Le nom ne peut pas être vide.",
+          },
+          notNull: { msg: "Le nom est une propriété requise." },
+        },
       },
       hp: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        // Validateur sequelize
+        validate: {
+          isInt: {
+            msg: "Utilisez uniquement des nombres entiers pour les points de vie.",
+          },
+          min: {
+            args: [0],
+            msg: "Les points de vie doivent être supérieurs ou égales à 0.",
+          },
+          max: {
+            args: [999],
+            msg: "Les points de vie doivent être inférieures ou égales à 999.",
+          },
+          notNull: { msg: "Les points de vie sont une propriété requise" },
+        },
       },
       cp: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        validate: {
+          isInt: {
+            msg: "Utilisez uniquement des nombres entiers pour les points de dégâts.",
+          },
+          min: {
+            args: [0],
+            msg: "Les points de dégâts doivent être supérieurs ou égales à 0.",
+          },
+          max: {
+            args: [99],
+            msg: "Les points de dégâts doivent être inférieures ou égales à 99.",
+          },
+          notNull: { msg: "Les cps sont une propriété requise" },
+        },
       },
       picture: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          isUrl: {
+            msg: "Utilisez uniquement une url valide pour l'image",
+          },
+          notNull: { msg: "Le liens vers l'image est requis" },
+        },
       },
       types: {
         type: DataTypes.STRING,
@@ -33,6 +89,26 @@ module.exports = (sequelize, DataTypes) => {
         },
         set(types) {
           this.setDataValue("types", types.join());
+        },
+        // Validateur personnalisé
+        validate: {
+          isTypesValid(value) {
+            if (!value) {
+              throw new Error("Un pokemon doit au moins avoir un type");
+            }
+            if (value.split(",").length > 3) {
+              throw new Error(
+                "Un pokémon ne peux pas avoir plus de trois types"
+              );
+            }
+            value.split(",").forEach((type) => {
+              if (!validTypes.includes(type)) {
+                throw new Error(
+                  `Le type d'un pokemon doit appartenir à la liste suivante : ${validTypes}`
+                );
+              }
+            });
+          },
         },
       },
     },
